0ef315e3a5382cb98fc4a9da6c44a2d5197d4cac
Ephrem <eakele@gmail.com>
2016-07-16 11:13:11 +1000

    Implement creating users profile for a project

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,17 +5,16 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-  profile = Profile.new(profile_params)
-        profile.user_id = current_user.user_id
-        profile.save
-        if poprofilest.save
+        @profile = Profile.new(profile_params)
+        @profile.user_id = current_user.id
+        if @profile.save
           flash[:notice] = "Profile successfully created"
           redirect_to '/'
         else
           redirect_to '/login'
         end
    end
-  def post_params
+  def profile_params
     params.require(:profile).permit(:first_name, :last_name, :address, :suburb, :state, :post_code)
   end
 end
