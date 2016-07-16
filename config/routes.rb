3df3ff94df82a5ff3644568d9ab150d79b4b5272
Ephrem <eakele@gmail.com>
2016-07-16 20:46:35 +1000

    routing problem fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root :to =>'main#index'
+  root :to =>'registrations#show'
   get '/registrations/new' => 'registrations#new'
   post '/registrations' => 'registrations#create'
   # get '/user/:id/registrations/show/' => 'registrations#show'
