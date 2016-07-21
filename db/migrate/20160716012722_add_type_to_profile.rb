15e744cdc3bc1176265c74fd0584a10dc40206f0
Ephrem <eakele@gmail.com>
2016-07-21 19:51:15 +1000

    database modification

--- db/migrate/20160716012722_add_type_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToProfile < ActiveRecord::Migration
-  def change
-    add_column :profiles, :type, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -77,19 +77,6 @@ ActiveRecord::Schema.define(version: 20160721023034) do
 
   add_index "institutions", ["cart_id"], name: "index_institutions_on_cart_id", using: :btree
 
-  create_table "profiles", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "address"
-    t.string   "suburb"
-    t.string   "state"
-    t.integer  "post_code"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "user_id"
-    t.string   "register_as"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
